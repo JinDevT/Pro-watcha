bf590247d2aaa2407f2dc3448c0b40cb2d67324e
김진태 <39721152+JinDevT@users.noreply.github.com>
2019-12-15 17:41:01 +0900

    Add files via upload

---
+++ src/component/start/Start.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import logoImg from '../../assets/images/logo.png';
+
+const StartTemplate = styled.div`
+    position: relative;
+    background: url('https://dhgywazgeek0d.cloudfront.net/watcha/image/upload/c_fill,h_1620,q_80,w_2880/v1564363966/crosgmb6yawejwkff87a.jpg') no-repeat center;
+    background-size: cover;
+    position: relative;
+    .inner {
+        height: 100vh;
+        width: 100%;
+        position: relative;
+        margin: 0 auto;
+        padding: 0 135px;
+    }
+    .area_text {
+        display: inline-block;
+        overflow: hidden;
+        position: relative;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        z-index: 100;
+    }
+    .logo {
+        display: inline-block;
+        width: 345px;
+        height: 125px;
+        background: url(${logoImg}) no-repeat ;
+        text-indent: -9999px;
+    }
+    .page_btn {
+        display: inline-block;
+        margin: 10px 0;
+        width: 290px;
+        height: 52px;
+        line-height: 52px;
+        border-radius: 30px;
+        color: #fff;
+    }
+    .page_btn.sign_up {
+        background-color: rgba(0,0,0,0.4);
+        border: 1px solid rgba(255,255,255,0.3);
+        &:hover {
+            background-color: #fff;
+            color: #000;
+        }
+    }
+    .page_btn.face_join {
+        background-color: #4065b3;
+    }
+    .sub_tit {
+        margin: 10px 0;
+        line-height: 1.3;
+        color: #e9ecef;
+    }
+
+    .sub_tit.face_info {
+        font-size: 15px;
+    }
+    .line_div {
+        width: 260px;
+        height: 1px;
+        background-color: #e9ecef;
+        margin: 0 auto 20px;
+    }
+    .sub_tit.watcha_info {
+        font-size: 17px;
+    }
+    .page_btn.sign_in {
+        background: #ff3376;
+    }
+    
+`;
+
+const Start = () => {
+    return(
+        <StartTemplate>
+            <div className="inner">
+                <div className="area_text">
+                    <Link to="/" className="logo">로고</Link>
+                    <br/>
+                    <div className="line_div"></div>
+                    <p className="sub_tit watcha_info">무료체험을 시작해보세요.</p>
+                    <Link to ="/main" className="page_btn sign_in">
+                        <span>무료체험 시작</span>
+                    </Link>
+                    <br/>
+                </div>
+            </div>
+        </StartTemplate>            
+    );
+}
+
+export default Start;
